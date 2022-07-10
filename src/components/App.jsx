8be8bbf406eefabda2e1d8c0873e359cdd9abed6
mradalteaha <59914727+mradalteaha@users.noreply.github.com>
2022-07-10 09:04:43 +0300

    first use of react component building seperated component , import and export practice

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Heading from "./Heading.jsx";
+import List from "./List.jsx";
+
+//start of the component tree
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+export default App;
